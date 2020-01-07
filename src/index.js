0af7e6643a74f5cc11b89b79ef4704e2b64608e4
jmknopf1007 <jmknopf1007@gmail.com>
2020-01-07 14:49:22 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
+let createTask = document.getElementById("create-task-form")
+let newInput = document.createElement("input")
+newInput.id = "new-task-due"
+newInput.placeholder = "due date"
+createTask.appendChild(newInput)
+
+let taskDescription = document.getElementById("new-task-description")
+
+
+
+
+function addTask(task, date){
+  let taskList = document.getElementById("tasks");
+  let li = document.createElement("li");
+  let listButton = document.createElement("button");
+  let editButton = document.createElement("button")
+  
+  editButton.innerText = "Edit"
+  listButton.innerText = "x"
+  li.textContent = `${task} - ${date}`
+  let textColor = document.querySelector("select")
+  li.style.color = textColor.value
+
+  taskList.appendChild(li);
+  taskList.appendChild(listButton);
+  taskList.appendChild(editButton);
+
+  editButton.addEventListener("click", function(){
+    let editInput = document.createElement("input")
+    editInput.value = li.innerText
+    let submitEditButton = document.createElement("button")
+    submitEditButton.textContent = "Save"
+    li.appendChild(submitEditButton)
+    li.appendChild(editInput)
+    submitEditButton.addEventListener("click", function(e){
+      li.innerText = editInput.value
+    })
+
+  });
+
+  listButton.addEventListener("click", function(){
+    li.remove();
+    listButton.remove();
+    editButton.remove()
+  })
+};
+
+function submitTask(event){
+  console.log(event)
+  let newDescription = event.target['new-task-description'].value
+  let newDueDate = event.target['new-task-due'].value
+  addTask(newDescription, newDueDate)
+}
+
+
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let createTask = document.getElementById("create-task-form")
+  createTask.addEventListener("submit", function(){
+    event.preventDefault();
+    submitTask(event)
+  })
 });
